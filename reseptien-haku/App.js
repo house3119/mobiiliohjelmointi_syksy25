01af13f5dbcf7cded39caf7891ff116ded8ff5d1
Antti Varis <ajvari88@gmail.com>
2025-09-08 19:27:57 +0300

    reseptien haku 1

---
+++ reseptien-haku/App.js
@@ -0,0 +1,128 @@
+import { StatusBar } from 'expo-status-bar';
+import { useRef, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable, FlatList, Image } from 'react-native';
+
+const mealApiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php'
+
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [list, setList] = useState([]);
+  const [searched, setSearched] = useState(false);
+
+  const queryInput = useRef(null);
+
+  const search = () => {
+    fetch(`${mealApiUrl}?i=${query}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.meals)
+      const holder = [];
+      if (data.meals) {
+        data.meals.forEach(element => {
+          holder.push(element)
+        });
+      }
+      setList(holder)
+      setSearched(true)
+      console.log(list)
+    })
+    .catch(err => console.error(err))
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        autoFocus={true}
+        value={query}
+        onChangeText={text => {setQuery(text);}}
+        style={styles.itemInput}
+        ref={queryInput} />
+
+      <View style={{flexDirection: 'row'}}>
+        <Pressable
+          style={styles.btn}
+          onPress={search}>
+          <Text style={styles.btnText}>
+            Find
+          </Text>
+        </Pressable>
+      </View>
+
+      <View>
+        {(list.length !== 0 && searched) ?
+          <FlatList
+            contentContainerStyle={styles.listContainer}
+            data={list}
+            renderItem={({item}) =>
+              <View style={styles.listItem}>
+                <Text style={{marginBottom: 2, fontWeight: 500}}>
+                  {item.strMeal}
+                </Text>
+                <Image
+                style={{height: 100, width: 100}}
+                  source={{
+                    uri: item.strMealThumb
+                  }}
+                />
+              </View>
+            }
+          />
+        : (list.length === 0 && searched) ?
+          <Text>
+            Nothing found
+          </Text>
+        : null}
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'start',
+    marginTop: 100
+  },
+  btn: {
+    width: 100,
+    backgroundColor: '#0d6efd',
+    marginTop: 15,
+    marginEnd: 5,
+    alignItems: 'center',
+    padding: 5,
+    borderRadius: 6
+  },
+  btnText: {
+    color: 'white'
+  },
+  itemInput: {
+    height: 40,
+    width: 250,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 6
+  },
+  listContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginTop: 12,
+    paddingTop: 4
+  },
+  listHeader: {
+    fontWeight: 700,
+    fontSize: 16,
+    color: 'blue',
+    marginBottom: 8
+  },
+  listItem: {
+    marginBottom: 4,
+    paddingBottom: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+    width: 300
+  }
+});
